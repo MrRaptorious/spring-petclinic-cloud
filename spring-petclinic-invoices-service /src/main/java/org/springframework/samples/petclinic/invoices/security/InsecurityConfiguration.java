6f6b73d1096780064d5d6f29650515f74d95c89c
MrRaptorious <35742529+MrRaptorious@users.noreply.github.com>
2025-05-30 21:57:14 +0200

    SECURITYYYYYY WIU🚨WIU🚨🚨🚨🚨🚨🚨🚨

--- spring-petclinic-invoices-service /src/main/java/org/springframework/samples/petclinic/invoices/security/InsecurityConfiguration.java
+++ spring-petclinic-invoices-service /src/main/java/org/springframework/samples/petclinic/invoices/security/InsecurityConfiguration.java
@@ -1,4 +1,4 @@
-package org.springframework.samples.petclinic.invoice.security;
+package org.springframework.samples.petclinic.invoices.security;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
